52700c339bb11be647896eb1c0c9caf8fdd0b0c1
Gavin Shaw <gshaw+github@pivotal.io>
2022-11-30 10:38:50 -0500

    Add roll endpoint and check for the top level json key

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World');
+app.use(express.json())
+
+app.post('/roll', function (req, res) {
+
+    if (req.body.dies) {
+        console.log('Dies key exists')
+        res.status(200).end()
+    }
+    else {
+        console.log('Dies key not found')
+        res.status(400).send("Required structure is:\n" +
+            "{\n" +
+            "\t\"dies\":\n" +
+            "\t[{\n" +
+            "\t    \"dieType\": int representing the number of sides,\n" +
+            "\t    \"rollCount\": int representing the number of rolls\n" +
+            "\t},{...}]" +
+            "\n}")
+    }
 })
 
 const server = app.listen(8081, function () {
